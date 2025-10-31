d859f4b262b50a9a948a96aa641a25238e466845
Jafethalv <jafeth.alvarado@ulatina.net>
2025-10-30 19:12:02 -0600

    calculadora
    trabajo

---
+++ Calculadora1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Calculadora1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba el primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Escriba el segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Elija la operación (+, -, *, /): ");
+            char operacion = Convert.ToChar(Console.ReadLine());
+
+            double resultado = 0;
+
+            if (operacion == '+')
+                resultado = num1 + num2;
+            else if (operacion == '-')
+                resultado = num1 - num2;
+            else if (operacion == '*')
+                resultado = num1 * num2;
+            else if (operacion == '/')
+            {
+                if (num2 != 0)
+                    resultado = num1 / num2;
+                else
+                    Console.WriteLine("Error: no se puede dividir entre cero.");
+            }
+            else
+            {
+                Console.WriteLine("Operación no válida.");
+                return;
+            }
+
+            // Mostrar el resultado
+            Console.WriteLine($"El resultado es: {resultado}");
+        }
+    }
+}
